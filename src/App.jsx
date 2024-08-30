7e6ff00323bd5ef327468e10f6a94b582dcc2503
BarabashTV <164335010+BarabashTV@users.noreply.github.com>
2024-08-30 23:49:55 +0300

    Destrukturointi tehty.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-const Hello = (props) => {
-  console.log(props);
+const Hello = ({ name, age, town }) => {
+  /*const name = props.name;
+  const age = props.age;
+  const town = props.town;*/
+  /*const { name, age, town } = props;*/
+
+  const bornYear = () => new Date().getFullYear() - age;
   return (
     <>
       <p>
-        Hello {props.name} from {props.town}. You are {props.age} years old.
+        Hello {name} from {town}. You are {age} years old.
       </p>
+      <p>So you are probably born {bornYear()}</p>
     </>
   );
 };
@@ -15,10 +21,12 @@ const Ystavat = ({ friends }) => {
       <section>
         <h2>Älä renderoi olioita</h2>
         <p>
-          Ensimmäinen ystävä {friends[0].name} on {friends[0].age} vuotta vanha.
+          Ensimmäinen ystävä {friends[0].name} from {friends[0].town} on{" "}
+          {friends[0].age} vuotta vanha.
         </p>
         <p>
-          Toinen ystävä {friends[1].name} on {friends[1].age} vuotta vanha.
+          Toinen ystävä {friends[1].name} from {friends[1].town} on{" "}
+          {friends[1].age} vuotta vanha.
         </p>
       </section>
     </>
@@ -43,10 +51,17 @@ const App = () => {
   console.log(now, a + b);
   const nimi = "Pekka";
   const ika = 10;
+  const town = "Vantaa";
   const friends = [
-    { name: "Leevi", age: 4 },
-    { name: "Venla", age: 10 },
+    { name: "Leevi", age: 4, town: "Rovaniemi" },
+    { name: "Venla", age: 10, town: "Karjaa" },
   ];
+  const props = {
+    name: "Maya",
+    age: 36,
+    town: "Oulu",
+  };
+
   return (
     <>
       <section>
@@ -60,11 +75,10 @@ const App = () => {
         <h1>Greetings</h1>
         <Hello name="Heikki" town="Helsinki" age="15" />
         <Hello name="Maija" town="Oulu" age={26 + 10} />
-        <Hello name={nimi} town="Juväskylä" age={ika} />
+        <Hello name={nimi} town={town} age={ika} />
       </section>
       <hr />
       <Ystavat friends={friends} />
-
       <Footer />
     </>
   );
